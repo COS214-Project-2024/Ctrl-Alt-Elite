902969ad01c2c9a1a15b409b4e3085988bfdbd3b
Raymond324 <84574562+Raymond324@users.noreply.github.com>
2024-10-29 21:40:25 +0200

    Delete public_services_command.cpp

--- public_services_command.cpp
+++
@@ -1,8 +0,0 @@
-// public_services_command.cpp
-#include "public_services_command.h"
-
-PublicServicesCommand::PublicServicesCommand(City& c, const std::string& svc) : city(c), service(svc) {}
-
-void PublicServicesCommand::execute() {
-    city.providePublicService(service);
-}
