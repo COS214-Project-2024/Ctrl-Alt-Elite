65d53452bfef5bfec8b107bc5ca0df7474b01248
Raymond324 <84574562+Raymond324@users.noreply.github.com>
2024-10-29 21:40:41 +0200

    Delete policy_command.cpp

--- policy_command.cpp
+++
@@ -1,8 +0,0 @@
-// policy_command.cpp
-#include "policy_command.h"
-
-PolicyCommand::PolicyCommand(City& c, const std::string& pol) : city(c), policy(pol) {}
-
-void PolicyCommand::execute() {
-    city.enforcePolicy(policy);
-}
