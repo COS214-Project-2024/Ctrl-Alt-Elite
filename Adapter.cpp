53676bc461ae8555ff20cee1709b77f11b788884
u19364742 <126067233+u19364742@users.noreply.github.com>
2024-10-29 15:07:38 +0200

    Add files via upload

---
+++ Adapter.cpp
@@ -0,0 +1,50 @@
+#include "Adapter.h"
+#include "Residential.h"
+#include "Commercial.h"
+#include "Industrial.h"
+#include "Landmark.h"
+
+#include <iostream>
+using namespace std;
+
+Adapter::Adapter(CompositeBuilding* cb){
+    adaptee = cb;
+}
+
+void Adapter::addResidentialUnits(int units){
+    for (int i = 0; i < units; i++) {
+        Residential* resBuilding = new Residential();
+        resBuilding->calculateOccupancyRate();
+        adaptee->addBuilding(resBuilding);
+        adaptee->increasePopulation(resBuilding->getNumResidents());
+    }
+}
+
+void Adapter::addCommercialUnits(int units){
+    for (int i = 0; i < units; i++) {
+        Commercial* comBuilding = new Commercial();
+        adaptee->addBuilding(comBuilding);
+        adaptee->increaseJobs(comBuilding->getEmploymentRating());
+    }
+}
+
+void Adapter::addIndustrialUnits(int units){
+    for (int i = 0; i < units; ++i) {
+        Industrial* indBuilding = new Industrial();
+        adaptee->addBuilding(indBuilding);
+        adaptee->increaseJobs(indBuilding->getEmploymentRating());
+    }
+}
+
+void Adapter::addLandmarkUnits(int units){
+    for (int i = 0; i < units; ++i) {
+        Landmark* landmarkBuilding = new Landmark();
+        adaptee->addBuilding(landmarkBuilding);
+        // Increase CompositeBuilding's entertainment rating based on each landmark's rating
+        adaptee->expandUtilities(landmarkBuilding->getEntertainmentRating());
+    }
+}
+
+void Adapter::displayCityStatus(){
+    adaptee->displayCityState();
+}
