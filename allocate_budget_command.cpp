689fb4db037d4ccca9af282934f5ca308295e804
Raymond324 <84574562+Raymond324@users.noreply.github.com>
2024-10-29 21:43:21 +0200

    Delete allocate_budget_command.cpp

--- allocate_budget_command.cpp
+++
@@ -1,9 +0,0 @@
-// allocate_budget_command.cpp
-#include "allocate_budget_command.h"
-
-AllocateBudgetCommand::AllocateBudgetCommand(City& c, const std::string& dept, double amt)
-        : city(c), department(dept), amount(amt) {}
-
-void AllocateBudgetCommand::execute() {
-    city.allocateBudget(department, amount);
-}
