9ef3f818f6032d1084017fa0502a97bd0d201a02
Raymond324 <84574562+Raymond324@users.noreply.github.com>
2024-11-02 23:53:56 +0200

    Add files via upload

--- cmake-build-debug/cmake_install.cmake
+++ cmake-build-debug/cmake_install.cmake
@@ -1,8 +1,8 @@
-# Install script for directory: C:/Ctrl-Alt-Elite
+# Install script for directory: C:/Users/jinfe/Downloads/Ctrl-Alt-Elite-Bablo (2)/Ctrl-Alt-Elite-Bablo
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
-  set(CMAKE_INSTALL_PREFIX "C:/Program Files (x86)/COS214_Project")
+  set(CMAKE_INSTALL_PREFIX "C:/Program Files (x86)/Ctrl_Alt_Elite_Bablo")
 endif()
 string(REGEX REPLACE "/$" "" CMAKE_INSTALL_PREFIX "${CMAKE_INSTALL_PREFIX}")
 
@@ -45,5 +45,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "C:/Ctrl-Alt-Elite/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "C:/Users/jinfe/Downloads/Ctrl-Alt-Elite-Bablo (2)/Ctrl-Alt-Elite-Bablo/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
